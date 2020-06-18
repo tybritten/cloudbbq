e09199be3cb5a817d369f03b4a4fa397bc6cfb52
Tyler Britten <1933680+tybritten@users.noreply.github.com>
2020-06-18 13:13:45 -0400

    more

--- tempHelper.js
+++ tempHelper.js
@@ -1,8 +1,8 @@
 module.exports = {
-    cToF:(c)=>{
-        return (c* 9 / 5 + 32)
+    cToF: (c) => {
+        return (c * 9) / 5 + 32
     },
-    fToC:(f)=>{
-        return (f - 32) * 5 / 9
-    }
+    fToC: (f) => {
+        return ((f - 32) * 5) / 9
+    },
 }
