8a23d9efd35d9ffa759ba5e6cbf5b67be6b800b9
Tyler Britten <1933680+tybritten@users.noreply.github.com>
2020-08-03 08:38:18 -0400

    updated for IBT2x

--- app.js
+++ app.js
@@ -1,171 +1,171 @@
-var noble = require('@abandonware/noble')
-var constHelper = require('./constHelper')
-var tempHelper = require('./tempHelper')
-var mqtt = require('mqtt')
-var config = require('config')
-var program = require('commander')
-var path = require('path')
-var GoogleAssistant = require('google-assistant')
+var noble = require("@abandonware/noble");
+var constHelper = require("./constHelper");
+var tempHelper = require("./tempHelper");
+var mqtt = require("mqtt");
+var config = require("config");
+var program = require("commander");
+var path = require("path");
+var GoogleAssistant = require("google-assistant");
 
-var mqttConfig = config.get('mqtt')
-var notificationConfig = config.get('notifications')
+var mqttConfig = config.get("mqtt");
+var notificationConfig = config.get("notifications");
 
-var mqttConnected = false
-var msgCount = 0
+var mqttConnected = false;
+var msgCount = 0;
 
 program
-    .version('1.1.0')
-    .option('-p1, --probe1 <n>', 'Probe 1 Target Temperature(F)', parseInt)
-    .option('-p2, --probe2 <n>', 'Probe 2 Target Temperature(F)', parseInt)
-    .parse(process.argv)
+  .version("1.1.0")
+  .option("-p1, --probe1 <n>", "Probe 1 Target Temperature(F)", parseInt)
+  .option("-p2, --probe2 <n>", "Probe 2 Target Temperature(F)", parseInt)
+  .parse(process.argv);
 
-notificationConfig.targets = [program.probe1, program.probe2]
-notificationConfig.set = [true, true]
+notificationConfig.targets = [program.probe1, program.probe2];
+notificationConfig.set = [false, false];
 
 const googleConfig = {
-    auth: {
-        keyFilePath: path.resolve(
-            __dirname,
-            notificationConfig.googleAssistant.oAuthSecretsFile
-        ),
-        // where you want the tokens to be saved
-        // will create the directory if not already there
-        savedTokensPath: path.resolve(__dirname, 'config/tokens.json'),
-    },
-}
+  auth: {
+    keyFilePath: path.resolve(
+      __dirname,
+      notificationConfig.googleAssistant.oAuthSecretsFile
+    ),
+    // where you want the tokens to be saved
+    // will create the directory if not already there
+    savedTokensPath: path.resolve(__dirname, "config/tokens.json"),
+  },
+};
 const assistant = notificationConfig.googleAssistant.enabled
-    ? new GoogleAssistant(googleConfig.auth)
-    : null
+  ? new GoogleAssistant(googleConfig.auth)
+  : null;
 
-var mqttConnString = `${mqttConfig.protocol}://${mqttConfig.username}:${mqttConfig.key}@${mqttConfig.url}`
-var client = mqtt.connect(mqttConnString)
+var mqttConnString = `${mqttConfig.protocol}://${mqttConfig.username}:${mqttConfig.key}@${mqttConfig.url}`;
+var client = mqtt.connect(mqttConnString);
 
-client.on('connect', () => {
-    mqttConnected = true
-})
+client.on("connect", () => {
+  mqttConnected = true;
+});
 
-noble.startScanning([], true)
+noble.startScanning([], true);
 
-var pairCharacteristic, tempCharacteristic, commandCharacteristic
+var pairCharacteristic, tempCharacteristic, commandCharacteristic;
 
-noble.on('discover', (peripheral) => {
-    // Check out this sample code: https://github.com/noble/noble/issues/179
-    if (peripheral.advertisement.localName === 'iBBQ') {
-        console.log('iBBQ Discovered')
-        noble.stopScanning()
+noble.on("discover", (peripheral) => {
+  // Check out this sample code: https://github.com/noble/noble/issues/179
+  if (peripheral.advertisement.localName === "iBBQ") {
+    console.log("iBBQ Discovered");
+    noble.stopScanning();
 
-        peripheral.on('disconnect', () => {
-            console.log('Lost connection to device.')
-            noble.startScanning()
-        })
+    peripheral.on("disconnect", () => {
+      console.log("Lost connection to device.");
+      noble.startScanning();
+    });
 
-        peripheral.connect((error) => {
-            if (error) {
-                console.error(error)
-            } else {
-                connectToIBBQ(peripheral)
-            }
-        })
-    }
-})
+    peripheral.connect((error) => {
+      if (error) {
+        console.error(error);
+      } else {
+        connectToIBBQ(peripheral);
+      }
+    });
+  }
+});
 
 function connectToIBBQ(peripheral) {
-    peripheral.discoverAllServicesAndCharacteristics(
-        (error, services, characteristics) => {
-            if (error) {
-                console.error(error)
-            } else {
-                for (let characteristic of characteristics) {
-                    switch (characteristic.uuid) {
-                    case 'fff2':
-                        pairCharacteristic = characteristic
-                        break
-                    case 'fff4':
-                        tempCharacteristic = characteristic
-                        break
-                    case 'fff5':
-                        commandCharacteristic = characteristic
-                        break
-                    }
-                }
-
-                pairToDevice()
-            }
+  peripheral.discoverAllServicesAndCharacteristics(
+    (error, services, characteristics) => {
+      if (error) {
+        console.error(error);
+      } else {
+        for (let characteristic of characteristics) {
+          switch (characteristic.uuid) {
+            case "fff2":
+              pairCharacteristic = characteristic;
+              break;
+            case "fff4":
+              tempCharacteristic = characteristic;
+              break;
+            case "fff5":
+              commandCharacteristic = characteristic;
+              break;
+          }
         }
-    )
+
+        pairToDevice();
+      }
+    }
+  );
 }
 
 function pairToDevice() {
-    pairCharacteristic.write(constHelper.autoPairKey(), true, (error) => {
-        if (error) {
-            console.error(error)
-        } else {
-            console.log('paired')
-            subscribeToEvents()
-        }
-    })
+  pairCharacteristic.write(constHelper.autoPairKey(), true, (error) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log("paired");
+      subscribeToEvents();
+    }
+  });
 }
 
 function subscribeToEvents() {
-    tempCharacteristic.subscribe((error) => {
-        if (error) {
-            console.error(error)
-        }
-    })
-    tempCharacteristic.on('data', (data) => handleTempEvent(data))
+  tempCharacteristic.subscribe((error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
+  tempCharacteristic.on("data", (data) => handleTempEvent(data));
 
-    console.log('setting units')
-    commandCharacteristic.write(constHelper.setUnitsFKey(), false)
-    console.log('sending start temp events')
-    commandCharacteristic.write(constHelper.startTempUpdates(), false)
+  console.log("setting units");
+  commandCharacteristic.write(constHelper.setUnitsFKey(), false);
+  console.log("sending start temp events");
+  commandCharacteristic.write(constHelper.startTempUpdates(), false);
 }
 
 function handleTempEvent(data) {
-    if (data && data.length == 4) {
-        var probeTemps = []
-        for (var i = 0; i < 2; i++) {
-            var rawTemp = data.readInt16LE(i * 2)
-            if (rawTemp != -10) {
-                probeTemps.push(tempHelper.cToF(rawTemp / 10))
-            } else {
-                probeTemps.push(null)
-            }
-        }
-        console.log(`Probes 1: ${probeTemps[0]}F 2: ${probeTemps[1]}F `)
-        if (mqttConnected) {
-            msgCount++
-            for (var j = 0; j < 6; j++) {
-                if (
-                    msgCount % mqttConfig.probeMessagePerPublish == 0 &&
-          probeTemps[j] != null
-                ) {
-                    client.publish(
-                        mqttConfig.topics[j],
-                        JSON.stringify({
-                            value: probeTemps[j],
-                        })
-                    )
-                }
-            }
-        }
-
-        if (notificationConfig.googleAssistant.enabled) {
-            for (var k = 0; k < 6; k++) {
-                if (notificationConfig.set[k] && notificationConfig.targets[k]) {
-                    if (probeTemps[k] > notificationConfig.targets[k]) {
-                        notificationConfig.set[k] = false
-                        sendGoogleNotification(k + 1, probeTemps[k])
-                    }
-                }
-            }
-        }
-    } else {
-        console.error('wierd empty or wrong size buffer')
+  if (data && data.length == 4) {
+    var probeTemps = [];
+    for (var i = 0; i < 2; i++) {
+      var rawTemp = data.readInt16LE(i * 2);
+      if (rawTemp != -10) {
+        probeTemps.push(tempHelper.cToF(rawTemp / 10));
+      } else {
+        probeTemps.push(null);
+      }
     }
+    console.log(`Probes 1: ${probeTemps[0]}F 2: ${probeTemps[1]}F `);
+    if (mqttConnected) {
+      msgCount++;
+      for (var j = 0; j < 6; j++) {
+        if (
+          msgCount % mqttConfig.probeMessagePerPublish == 0 &&
+          probeTemps[j] != null
+        ) {
+          client.publish(
+            mqttConfig.topics[j],
+            JSON.stringify({
+              value: probeTemps[j],
+            })
+          );
+        }
+      }
+    }
+
+    if (notificationConfig.googleAssistant.enabled) {
+      for (var k = 0; k < 6; k++) {
+        if (notificationConfig.set[k] && notificationConfig.targets[k]) {
+          if (probeTemps[k] > notificationConfig.targets[k]) {
+            notificationConfig.set[k] = false;
+            sendGoogleNotification(k + 1, probeTemps[k]);
+          }
+        }
+      }
+    }
+  } else {
+    console.error("wierd empty or wrong size buffer");
+  }
 }
 
 function sendGoogleNotification(probe, temp) {
-    assistant.start({
-        textQuery: `broadcast Probe ${probe} has reached ${temp} degrees fahrenheit.`,
-    })
+  assistant.start({
+    textQuery: `broadcast Probe ${probe} has reached ${temp} degrees fahrenheit.`,
+  });
 }
